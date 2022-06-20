a0c3b06d1fb789aa1925fcb4636dae384fd8d84b
NicoWoch <woch.nico@gmail.com>
2022-06-20 16:28:20 +0200

    Add basic tests to tests_branch

---
+++ test/first_test.py
@@ -0,0 +1,14 @@
+import unittest
+import to_test
+
+
+class MyTestCase(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(to_test.add(2, 2), 4)
+
+    def test_sub(self):
+        self.assertEqual(to_test.sub(8, 4), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
