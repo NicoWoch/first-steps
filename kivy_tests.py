2136a73f87b21ad60874f1f388046aee92055cb4
BuildTools <unconfigured@null.spigotmc.org>
2022-06-20 16:01:23 +0200

    Added some test of kivy framework

---
+++ kivy_tests.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class TestApp(App):
+    def build(self):
+        return MainWidget()
+
+
+
+if __name__ == '__main__':
+    TestApp().run()
