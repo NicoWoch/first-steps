903466fdc562039af846b3cfa787f0dc740a3684
NicoWoch <woch.nico@gmail.com>
2022-06-20 17:24:10 +0200

    Added div function

--- test/test_1.py
+++ test/test_1.py
@@ -9,6 +9,9 @@ class MyTestCase(unittest.TestCase):
     def test_sub(self):
         self.assertEqual(to_test.sub(8, 4), 4)
 
+    def test_div(self):
+        self.assertEqual(to_test.div(4, 2), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
